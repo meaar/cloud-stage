d7ddd93fbf0ec6ad74f376f10db2c5d682fd93db
meaar <alberto.ribeiro@aluno.ufca.edu.br>
2023-05-26 19:29:18 -0300

    added initial context menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,16 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.oncontextmenu = rightClick;
+
+function rightClick(e) {
+    e.preventDefault();
+    let menu = document.getElementById("context");
+    if (menu.style.display == "block") {
+        menu.style.display = "none"
+    } else {
+        menu.style.display = 'block';
+        menu.style.left = e.pageX + "px";
+        menu.style.top = e.pageY + "px";
+    }
+} 
